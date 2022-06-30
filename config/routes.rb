b5d4138c5c70eec456599a5a8ee0e0dd79ef8a3a
Vladimir Ryshchenko <vr@rsdigital.ru>
2022-06-30 10:56:15 +0300

    add simple form gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages', to: 'static_pages#landing_page'
-  get 'static_pages/privacy_policy'
+  resources :courses
+  root to: "static_pages#landing_page"
+  get 'privacy_policy', to: "static_pages#privacy_policy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
